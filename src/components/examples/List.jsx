beebb15a22a19ac2f24569896fd93b9339b35ab2
tom <caolvchong@gmail.com>
2018-01-01 20:23:21 +0800

    update style

--- src/components/examples/List.jsx
+++ src/components/examples/List.jsx
@@ -6,26 +6,42 @@ export default props => {
   return (
     <ul className={style.list}>
       <li>
-        <Link to={urlFor('examples.constant')}>不同环境下的常量示例</Link>
+        <Link className={style.link} to={urlFor('examples.constant')}>
+          不同环境下的常量示例
+        </Link>
       </li>
       <li>
-        <Link to={urlFor('examples.action')}>action使用示例：计数器</Link>
+        <Link className={style.link} to={urlFor('examples.action')}>
+          action使用示例：计数器
+        </Link>
       </li>
       <li>
-        <Link to={urlFor('examples.params', { id: 233, name: 'robot' })}>路由URL变量和参数例子</Link>
+        <Link className={style.link} to={urlFor('examples.params', { id: 233, name: 'robot' })}>
+          路由URL变量和参数例子
+        </Link>
       </li>
       <li>
-        <Link to={urlFor('examples.request')}>请求示例（CORS方式）</Link>
+        <Link className={style.link} to={urlFor('examples.request')}>
+          请求示例（CORS方式）
+        </Link>
       </li>
       <li>
-        <Link to={urlFor('examples.permission')}>权限拦截：计数器需要大于10才能访问</Link>
+        <Link className={style.link} to={urlFor('examples.permission')}>
+          权限拦截：计数器需要大于10才能访问
+        </Link>
       </li>
       <li>
-        <Link to={'/examples/not-found'}>404 page (子路由)</Link>
-        <Link to={'/path/to/not-found'}>404 page (全局)</Link>
+        <Link className={style.link} to={'/examples/not-found'}>
+          404 page (子路由)
+        </Link>
+        <Link className={style.link} to={'/path/to/not-found'}>
+          404 page (全局)
+        </Link>
       </li>
       <li>
-        <Link to={urlFor('examples.ui')}>UI组件例子</Link>
+        <Link className={style.link} to={urlFor('examples.ui')}>
+          UI组件例子
+        </Link>
       </li>
     </ul>
   );
