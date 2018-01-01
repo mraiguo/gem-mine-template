c8730248d6c1542dd6486a3d991ac44e76f99679
tom <caolvchong@gmail.com>
2018-01-01 20:25:52 +0800

    update style

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -9,7 +9,7 @@ export default props => {
       <div>
         <img className={style.logo} src={logo} alt="logo" />
       </div>
-      <h2>welcome to gem-mine! </h2>
+      <h2 className={style.title}>welcome to gem-mine! </h2>
       <p>希望在这里可以挖到属于你的宝藏 💎</p>
       <Link className={style.examples} to={urlFor('examples')}>
         这里有一些简单的例子 go!
