daf8a059260bd827439714a26ca4ae556ba987a3
tom <caolvchong@gmail.com>
2018-01-01 21:15:55 +0800

    default support alias for fish

--- config/webpack/helper.js
+++ config/webpack/helper.js
@@ -222,7 +222,8 @@ const helper = {
         config: CONFIG,
         components: path.resolve(SRC, 'components'),
         styles: path.resolve(SRC, 'styles'),
-        global: path.resolve(SRC, 'global')
+        global: path.resolve(SRC, 'global'),
+        fish: '@sdp.nd/fish'
       }
     };
     if (params.extensions) {
