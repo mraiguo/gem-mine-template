6a19f411acf83779dd79d1b820c9b4bc9ce9096f
tom <caolvchong@gmail.com>
2018-01-01 20:28:37 +0800

    update style

--- src/components/examples/permission/Y.jsx
+++ src/components/examples/permission/Y.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { urlFor, Link } from 'cat-eye';
+import style from '../style';
 
 export default props => {
   return (
     <div>
       <span>yyyyy </span>
-      <Link to={urlFor('examples.permission.x')}>goto page X</Link>
+      <Link className={style.link} to={urlFor('examples.permission.x')}>
+        goto page X
+      </Link>
     </div>
   );
 };
