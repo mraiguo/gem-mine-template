d793a37e800f0285903eb0372985fb5f5d42e08c
tom <caolvchong@gmail.com>
2018-01-09 13:25:03 +0800

    remove some config

--- config/webpack.js
+++ config/webpack.js
@@ -19,12 +19,7 @@ exports.vendor = []
 // exports.port = 9000;
 
 // 自定义的 loaders
-exports.loaders = [
-  {
-    test: /\.md$/,
-    loader: 'html-loader!markdown-highlight-loader?sanitize=false'
-  }
-]
+exports.loaders = []
 
 // 自定义的 plugins
 exports.plugins = []
@@ -32,4 +27,4 @@ exports.plugins = []
 // 额外需要从 public 目录拷贝到编译后目录的资源（例如一些第三方的lib）
 // 只需要写文件文件名即可，会在 vendor 构造时进行拷贝
 // 引用请自行修改 public/index.html 进行处理
-exports.additional = ['jquery-1.9.0.js']
+exports.additional = []
