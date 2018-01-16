27d10345a860bbd2b3de3ae7f24825014599f456
tom <caolvchong@gmail.com>
2018-01-16 13:33:42 +0800

    format code

--- config/webpack.js
+++ config/webpack.js
@@ -7,16 +7,16 @@ exports.resolve = {
 }
 
 // 编译目录配置
-// exports.buildPath = path.resolve(__dirname, '../build');
+// exports.buildPath = path.resolve(__dirname, '../build')
 
 // todo: 静态资源路径配置, 如果有 cdn 支持，可以配置到 cdn，对本地开发无效
-// exports.publicPath = './';
+// exports.publicPath = './'
 
 // 加入 vendor 公共包的库
 exports.vendor = []
 
 // webpack dev server 默认端口，也可以通过命令行来指定 --port=9000，默认9000
-// exports.port = 9000;
+// exports.port = 9000
 
 // 自定义的 loaders
 exports.loaders = []
