571c64961ee583518719675f4f2f10317efdf2ec
tom <caolvchong@gmail.com>
2018-01-18 10:25:23 +0800

    automatic change wds port when it in use

--- config/webpack/dev.js
+++ config/webpack/dev.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const detect = require('detect-port')
+const chalk = require('chalk')
 const { helper, SRC, join } = require('./helper')
 
 const isHot = process.env.npm_config_hot !== ''
@@ -6,53 +8,68 @@ const shouldAnalyzer = !!process.env.npm_config_analyzer
 
 const custom = require('../webpack')
 
-const config = {
-  entry: {
-    polyfill: ['babel-polyfill'],
-    main: [path.resolve(SRC, 'index.js')]
-  },
-  output: helper.output.hash(),
-  resolve: helper.resolve(),
-  resolveLoader: helper.resolveLoader(),
-  devtool: 'source-map',
-  module: {
-    loaders: join(
-      helper.loaders.babel(isHot),
-      helper.loaders.css(isHot),
-      helper.loaders.less(isHot),
-      helper.loaders.sass(isHot),
-      helper.loaders.source(),
-      custom.loaders
-    )
-  },
-  plugins: join(
-    helper.plugins.define('dev', {
-      DEBUG: true
-    }),
-    helper.plugins.ignore(/vertx/),
-    helper.plugins.scopeHosting(),
-    helper.plugins.extractCss(),
-    helper.plugins.splitCss(),
-    helper.plugins.html(),
+const configPromise = new Promise(function (resolve, reject) {
+  const config = {
+    entry: {
+      polyfill: ['babel-polyfill'],
+      main: [path.resolve(SRC, 'index.js')]
+    },
+    output: helper.output.hash(),
+    resolve: helper.resolve(),
+    resolveLoader: helper.resolveLoader(),
+    devtool: 'source-map',
+    module: {
+      loaders: join(
+        helper.loaders.babel(isHot),
+        helper.loaders.css(isHot),
+        helper.loaders.less(isHot),
+        helper.loaders.sass(isHot),
+        helper.loaders.source(),
+        custom.loaders
+      )
+    },
+    plugins: join(
+      helper.plugins.define('dev', {
+        DEBUG: true
+      }),
+      helper.plugins.ignore(/vertx/),
+      helper.plugins.scopeHosting(),
+      helper.plugins.extractCss(),
+      helper.plugins.splitCss(),
+      helper.plugins.html(),
 
-    custom.plugins,
-    helper.plugins.done()
-  ),
-  stats: {
-    children: false,
-    colors: true
+      custom.plugins,
+      helper.plugins.done()
+    ),
+    stats: {
+      children: false,
+      colors: true
+    }
   }
-}
 
-if (isHot) {
-  config.plugins.push(helper.plugins.hot())
-}
-if (shouldAnalyzer) {
-  config.plugins.push(helper.plugins.analyzer())
-}
+  if (isHot) {
+    config.plugins.push(helper.plugins.hot())
+  }
+  if (shouldAnalyzer) {
+    config.plugins.push(helper.plugins.analyzer())
+  }
 
-const devServer = helper.devServer(isHot)
-config.plugins.push(helper.plugins.browser(`http://${devServer.host}:${devServer.port}`))
-config.devServer = devServer
+  let port = process.env.npm_config_port || config.port || 9000
+  detect(port)
+    .then(function (p) {
+      if (port !== p) {
+        console.log(chalk.cyanBright(`warning: port ${port} has been used, will use the ${p} instead.\n`))
+        port = p
+      }
+      const devServer = helper.devServer(isHot, port)
+      config.plugins.push(helper.plugins.browser(`http://${devServer.host}:${devServer.port}`))
+      config.devServer = devServer
+      resolve(config)
+    })
+    .catch(function (err) {
+      console.log(err)
+      process.exit(0)
+    })
+})
 
-module.exports = config
+module.exports = configPromise
