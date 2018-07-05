ab4daae7d2e3b12b657a7971da68624606f1212d
tom <caolvchong@gmail.com>
2018-07-06 02:08:44 +0800

    lint when git commit

--- config/webpack/scripts/lint.js
+++ config/webpack/scripts/lint.js
@@ -6,10 +6,13 @@ const print = require('../helper/print')
 const root = path.resolve(__dirname, '../../../')
 const fix = process.env.npm_config_fix ? '--fix' : ''
 
+let error
 const exec = function (cmd) {
   try {
     return execSync(cmd, { stdio: [process.stdin, process.stdout, process.stderr] })
-  } catch (e) {}
+  } catch (e) {
+    error = true
+  }
 }
 
 exec(`stylelint "${root}/src/**/*.css" ${fix}`)
@@ -17,8 +20,12 @@ exec(`stylelint "${root}/src/**/*.scss" ${fix}`)
 exec(`stylelint "${root}/src/**/*.less" ${fix}`)
 exec(`eslint ${fix} --ext .js,.jsx ${root}/src ${root}/config `)
 
-if (fix) {
-  print.info('部分错误已经自动修正，剩余的请手动进行处理。可以再次运行 npm run lint 进行检查')
+if (error) {
+  if (fix) {
+    print.info('部分错误已经自动修正，剩余的请手动进行处理。可以再次运行 npm run lint 进行检查')
+  } else {
+    print.warning('可以尝试运行 npm run lint --fix 进行错误自动修复')
+  }
 } else {
-  print.warning('可以尝试运行 npm run lint --fix 进行错误自动修复')
+  print.info('完美！代码规范检查没有发现任何错误信息!')
 }
